8cc7ff3d708b89da78f4556906936f66a00441aa
heeraku <77922980+heeraku@users.noreply.github.com>
2021-08-26 12:08:29 +0900

    Add files via upload

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg"];
+const images = ["0.webp", "1.webp", "2.webp", "3.webp", "4.webp", "5.webp", "6.webp", "7.webp", "8.webp", "9.webp", "10.webp"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
